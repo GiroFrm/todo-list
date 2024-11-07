4649a4da6a31101c4bd74ae9067d68d3c91394f5
GiroFrm <girofram844@gmail.com>
2024-11-07 20:04:16 +1100

    add: Add createProject function with methods to add, remove, and get todos

---
+++ src/createProject.js
@@ -0,0 +1,19 @@
+
+export default function createProject(name) {
+  let todos =[]
+  return {
+    name,
+    addTodo(todo) {
+        if(todos.find(element=> element.title === todo.title )) return 
+        todos.push(todo)
+    },
+    removeTodo(todo){
+        todos = todos.filter(element => element.title !== todo.title);
+        
+    },
+
+    getTodos() {
+        return[...todos]
+    }
+  }
+}
