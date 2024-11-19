002d7d8104b1f1bcbda158b91ab7ffe70b7feb57
GiroFrm <girofram844@gmail.com>
2024-11-19 22:47:23 +1100

    refactor: Import and use setupTodosProject in domRenderer.js
    - Imported setupTodosProject from setupEventListeners.js
    - Improved project setup handling in renderProjects function

--- src/domRenderer.js
+++ src/domRenderer.js
@@ -2,41 +2,41 @@
 import appController from "./appController";
 import { setUpEditForm, setupTodosProject } from "./setupEventListeners";
 import { format, parseISO } from 'date-fns';
+import { createTodoElement } from "./todoElement";
 
-export const containerAddProject = document.querySelector('.formAddProject');
-             
- export const btnProject = document.querySelector('.project-btn');
+// export const containerAddProject = document.querySelector('.formAddProject');           
+//  export const btnProject = document.querySelector('.project-btn');
  
-    btnProject.addEventListener('click',()=>{
-        if (containerAddProject.style.display === 'none') {
-            containerAddProject.style.display = 'block';
-        } else {
-            containerAddProject.style.display = 'none';
-        }
-    });
+//     btnProject.addEventListener('click',()=>{
+//         if (containerAddProject.style.display === 'none') {
+//             containerAddProject.style.display = 'block';
+//         } else {
+//             containerAddProject.style.display = 'none';
+//         }
+//     });
 
 
-     document.getElementById('projectForm').onsubmit = function(event) {
-        event.preventDefault(); // Prevent the form from submitting the traditional way
-        const projectName = document.getElementById('projectNameInput').value;
-       createNewProject(projectName);
+//      document.getElementById('projectForm').onsubmit = function(event) {
+//         event.preventDefault(); // Prevent the form from submitting the traditional way
+//         const projectName = document.getElementById('projectNameInput').value;
+//        createNewProject(projectName);
        
-        document.getElementById('projectNameInput').value= '';
+//         document.getElementById('projectNameInput').value= '';
        
-    };
+//     };
 
-    document.getElementById('cancelButton').onclick = function() {
-        document.getElementById('projectNameInput').value = ''; // Clear the input field
-        document.querySelector('.formAddProject').style.display = 'none'; // Hide the form
-    };
+//     document.getElementById('cancelButton').onclick = function() {
+//         document.getElementById('projectNameInput').value = ''; // Clear the input field
+//         document.querySelector('.formAddProject').style.display = 'none'; // Hide the form
+//     };
 
-     
-  
-    function createNewProject(name) {
-        appController.addNewProject(name);
-        // updateProjectsList
-        renderProjects();
-    }
+   
+   
+    // function createNewProject(name) {
+    //     appController.addNewProject(name);
+    //     // updateProjectsList
+    //     renderProjects();
+    // }
 
   export function renderProjects() {
         const projectsContainer =  document.querySelector('.projects-container');
@@ -54,124 +54,37 @@ export const containerAddProject = document.querySelector('.formAddProject');
 
   }
 
-  export function renderTodos(projectName='Home1') {
+  export function renderTodos(projectName = "Home1") {
+    const projectSection = document.querySelector(".project-section");
+    projectSection.innerHTML = " ";
 
-    document.querySelector('.project-section').innerHTML = ' ';
+    const project = appController
+      .getProjectsList()
+      .find((project) => project.name === projectName);
 
-      const project = appController.getProjectsList().find(project=> project.name === projectName);
-       
-         project.getTodos().forEach(todo=>{
-          
-            const todoContainer = document.createElement('div');
-            todoContainer.classList.add('todo-container');
-            const todoHeader = document.createElement('div');
-            todoHeader.classList.add('todo-header');
-            todoContainer.appendChild(todoHeader);
-            const label = document.createElement('label');
-            label.classList.add('container');
-            const date = document.createElement('p');
-            date.classList.add('date-todo');
-            date.innerHTML='date';
-            todoHeader.appendChild(label);
-            todoHeader.appendChild(date);
-            const titleTodo = document.createElement('p');
-            titleTodo.innerHTML = todo.title;
-            const input = document.createElement('input');
-            input.type="checkbox";
-            const span = document.createElement('span');
-            span.classList.add('checkmark');
-            label.appendChild(input);
-            label.appendChild(span);
-            label.appendChild(titleTodo);
-            const edit = document.createElement('p');
-            edit.innerHTML='edit';
-            const cancel = document.createElement('p');
-            cancel.innerHTML = 'delete';
-            cancel.classList.add('todo-delete');
-            edit.style.cursor="pointer"
-            cancel.style.cursor="pointer";
-            todoContainer.appendChild(edit);
-            todoContainer.appendChild(cancel);
+    project.getTodos().forEach((todo) => {
+      const todoContainer = createTodoElement(todo, project);
+      projectSection.appendChild(todoContainer);
+    });
+  }
 
-            document.querySelector('.project-section').appendChild(todoContainer);
-           
-            if(todo.dueDate){
-            const dateNumber = parseISO(todo.dueDate); 
-            const dayNumber = format(dateNumber, 'd');
-            const abbreviatedMonthName = format(dateNumber, 'MMM');
-            date.innerHTML = `${abbreviatedMonthName} ${dayNumber}`
-            }
-            
-            input.addEventListener('change', ()=>{
-
-                if(input.checked) {
-                    console.log('is checked');
-                    //apply line throught  todo
-                    edit.style.display="none"
-                    cancel.style.display="none";
-                    todoHeader.style.textDecoration=" line-through";
-                    todo.toggleCompleted();
-                    
-                } else {
-                    console.log('check is uncheck')
-                      edit.style.display="block";
-                      cancel.style.display="block";
-                      todoHeader.style.textDecoration=" none";
-                      todo.toggleCompleted();
-                    
-                }
-            })
-            
-            cancel.addEventListener('click', ()=>{
-                project.removeTodo(todo);
-                renderTodos(project.name)
-            })
-
-            edit.addEventListener("click", ()=> {
-                
-                document.querySelector('#editFormTodo').style.display="block";
-
-                const formEditTask = document.querySelector('.addEditTaskform');
-                formEditTask.style.display="block";
-                document.getElementById('titleInputEdit').value = todo.title;
-                document.getElementById('descriptionInputEdit').value = todo.description;
-              
-                setUpEditForm(todo.title, project.name, todo.priority, todo.dueDate);
-             
-            })
-         })
-    }
-
-
+  
    export function renderSelectProjects() {
-        //get list of project
-        const projectsList  = appController.getProjectsList(); 
-        // get form add new todos
-        const selectMenu = document.querySelector('#projectSelect');  
-             selectMenu.innerHTML = ''     
-        // for each project build an option element
-        projectsList.forEach((project)=>{
-            const option = document.createElement('option');
-                option.value = project.name;
-                option.innerHTML = project.name;
-                selectMenu.appendChild(option);
-            
-        })
-    }
+     const projectsList = appController.getProjectsList();
+     populateSelectedMenu(projectsList, document.querySelector("#projectSelect"))
+   }
 
     export function renderSelectEditProjects() {
-        //get list of project
-        const projectsList  = appController.getProjectsList(); 
-        // get form add new todos
-        const selectMenu = document.querySelector('#projectSelectEdit');  
-             selectMenu.innerHTML = '';
-        // for each project build an option element
-        projectsList.forEach((project)=>{
-            const option = document.createElement('option');
-                option.value = project.name;
-                option.innerHTML = project.name;
-                selectMenu.appendChild(option);
-              //  selectMenuEdit.appendChild(option);
-        })
+      const projectsList = appController.getProjectsList();
+      populateSelectedMenu(projectsList, document.querySelector("#projectSelectEdit"))
     }
   
+     function populateSelectedMenu(projectsList, selectMenu) {
+        selectMenu.innerHTML = '';
+        projectsList.forEach((project) => {
+            const option = document.createElement("option");
+            option.value = project.name;
+            option.innerHTML = project.name;
+            selectMenu.appendChild(option);
+          });
+     }
