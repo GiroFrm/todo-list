20ac5c2d5830f31342127bb9b95232387c1a75d5
GiroFrm <girofram844@gmail.com>
2024-11-19 16:31:29 +1100

    feat: Add utils.js for utility functions
    - Added formatDueDate function to format and log due dates
    - Added resetForm function to reset forms after submission
    - Improved code modularity and reusability by moving common operations to utils.js

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+import { format, parseISO } from 'date-fns'; 
+export function formatDueDate(dueDate) {
+     const date = parseISO(dueDate); 
+     const dayNumber = format(date, 'd'); 
+     const abbreviatedMonthName = format(date, 'MMM'); 
+   
+}
+    
+    export function resetForm(form) { 
+        form.reset(); 
+    }
