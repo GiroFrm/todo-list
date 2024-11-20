3559b76b802adfced39503a085e926b401366f4f
GiroFrm <girofram844@gmail.com>
2024-11-20 22:21:06 +1100

    refactor: Update renderSelectPRoject to accept formTask parameter

--- src/domRenderer.js
+++ src/domRenderer.js
@@ -1,42 +1,9 @@
 
 import appController from "./appController";
-import { setUpEditForm, setupTodosProject } from "./setupEventListeners";
-import { format, parseISO } from 'date-fns';
+import { setupTodosProject } from "./setupEventListeners";
+
 import { createTodoElement } from "./todoElement";
 
-// export const containerAddProject = document.querySelector('.formAddProject');           
-//  export const btnProject = document.querySelector('.project-btn');
- 
-//     btnProject.addEventListener('click',()=>{
-//         if (containerAddProject.style.display === 'none') {
-//             containerAddProject.style.display = 'block';
-//         } else {
-//             containerAddProject.style.display = 'none';
-//         }
-//     });
-
-
-//      document.getElementById('projectForm').onsubmit = function(event) {
-//         event.preventDefault(); // Prevent the form from submitting the traditional way
-//         const projectName = document.getElementById('projectNameInput').value;
-//        createNewProject(projectName);
-       
-//         document.getElementById('projectNameInput').value= '';
-       
-//     };
-
-//     document.getElementById('cancelButton').onclick = function() {
-//         document.getElementById('projectNameInput').value = ''; // Clear the input field
-//         document.querySelector('.formAddProject').style.display = 'none'; // Hide the form
-//     };
-
-   
-   
-    // function createNewProject(name) {
-    //     appController.addNewProject(name);
-    //     // updateProjectsList
-    //     renderProjects();
-    // }
 
   export function renderProjects() {
         const projectsContainer =  document.querySelector('.projects-container');
@@ -68,19 +35,18 @@ import { createTodoElement } from "./todoElement";
     });
   }
 
-  
-   export function renderSelectProjects() {
-     const projectsList = appController.getProjectsList();
-     populateSelectedMenu(projectsList, document.querySelector("#projectSelect"))
-   }
+   export function renderSelectProjects(formTask) {
+    const projectsList = appController.getProjectsList();
+    populateSelectedMenu(projectsList, formTask.querySelector("#projectSelect"))
+  }
 
     export function renderSelectEditProjects() {
       const projectsList = appController.getProjectsList();
       populateSelectedMenu(projectsList, document.querySelector("#projectSelectEdit"))
     }
-  
+    
      function populateSelectedMenu(projectsList, selectMenu) {
-        selectMenu.innerHTML = '';
+       selectMenu.innerHTML = '';
         projectsList.forEach((project) => {
             const option = document.createElement("option");
             option.value = project.name;
