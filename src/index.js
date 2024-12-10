622de7a8f86496b12956561e989fe14f1edd4ab5
GiroFrm <girofram844@gmail.com>
2024-12-10 14:20:48 +1100

    feat: Add class navBar to load eventListener navBar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import './style.css'
+import './style.css';
+import createTodo from './createTodo';
+import createProject from './createProject';
+import appController from './appController';
+import {renderProjects, renderTodos} from './domRenderer';
+import { setupTodosProject, setUpFormTodo, setupEventListeners,  setupAddProjectForm, setUpAddTask } from './setupEventListeners';
+import { navBar } from './navMenu';
+
+
+
+
+ window.onload = () => { // conditionally runs block on whether window is loaded - event listeners don't run until elements are on the page. (avoid timing conflicts).
+    renderProjects();
+    setupAddProjectForm();
+     renderTodos();
+     setupTodosProject();
+     setUpAddTask();
+     navBar();
+}
+
+
+
+
+
